8cf76a052c7cfd059611380ff8ece16f1b6686f1
Sharun 1 <sharun.1@vml.com>
2025-01-13 08:04:33 +0530

    font styling extension added

--- app/workspace/[fileId]/page.js
+++ app/workspace/[fileId]/page.js
@@ -5,6 +5,7 @@ import WorkspaceHeader from '../_components/WorkspaceHeader'
 import { useQueries, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import PdfViewer from '../_components/PdfViewer';
+import TextEditor from '../_components/TextEditor';
 
 function Workspace() {
     const {fileId}=useParams();
@@ -21,6 +22,7 @@ function Workspace() {
         <div className='grid grid-cols-1  md:grid-cols-2 gap-5'>
             <div>
                 {/* Text Editor  */}
+                 <TextEditor/>
             </div>
             <div>
                 {/* PDF Viewer */}
