ee0ae269c75c50b9f38a4bc5be0412f906050ce8
Sharun 1 <sharun.1@vml.com>
2025-01-13 07:20:09 +0530

    pdf viewer added

--- app/workspace/[fileId]/page.js
+++ app/workspace/[fileId]/page.js
@@ -1,23 +1,30 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 import WorkspaceHeader from '../_components/WorkspaceHeader'
+import { useQueries, useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import PdfViewer from '../_components/PdfViewer';
 
 function Workspace() {
     const {fileId}=useParams();
-const GetFileInfo=()=>{
+    const fileInfo=useQuery(api.fileStorage.GetFileRecord,{
+        fileId:fileId
+    });
+    useEffect(()=>{
+      console.log(fileInfo)
+    },[fileInfo])
 
-    }
   return (
     <div>
-        <WorkspaceHeader/>
-        <div className='flex justify-between items-center'>
+        <WorkspaceHeader  />
+        <div className='grid grid-cols-1  md:grid-cols-2 gap-5'>
             <div>
                 {/* Text Editor  */}
             </div>
             <div>
                 {/* PDF Viewer */}
-
+                <PdfViewer  fileUrl={fileInfo?.fileUrl}  />
             </div>
         </div>
     </div>
