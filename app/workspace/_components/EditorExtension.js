e6321bf67052e93b69701f79a9fb6c5f03a7f3a1
Sharun 1 <sharun.1@vml.com>
2025-01-24 02:19:36 +0530

    extension added

--- app/workspace/_components/EditorExtension.js
+++ app/workspace/_components/EditorExtension.js
@@ -1,26 +1,88 @@
-import { Bold, Italic } from "lucide-react";
+import {
+  Bold,
+  CodeIcon,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  ListIcon,
+  UnderlineIcon,
+ 
+} from "lucide-react";
 import React from "react";
 
+
+
 function EditorExtension({ editor }) {
-  return editor &&(
-    <div className="p-5 ">
-      <div className="control-group">
-        <div className="button-group flex gap-3">
-          <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "text-blue-500" : ""}
+  return (
+    editor && (
+      <div className="p-5 ">
+        <div className="control-group">
+          <div className="button-group flex gap-3">
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 1 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading1 />
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 2 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading2 />
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 3 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 3 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading3 />
+            </button>
+            {/* <button
+              onClick={() => editor.chain().focus().toggleUnderline().run()}
+              className={editor.isActive("underline") ? "text-blue-500" : ""}
+            >
+              <UnderlineIcon/>
+            </button> */}
+             <button
+            onClick={() => editor.chain().focus().toggleCode().run()}
+            className={editor.isActive('code') ? 'text-blue-500' : ''}
           >
-            <Bold />
+            <CodeIcon/>
           </button>
           <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            className={editor.isActive('italic') ? "text-blue-500": ''}
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'text-blue-500' : ''}
           >
-           <Italic/>
+            <ListIcon/>
           </button>
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "text-blue-500" : ""}
+            >
+              <Bold />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "text-blue-500" : ""}
+            >
+              <Italic />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
