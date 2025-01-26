c8aa890225b276903a2207c41e7db27c0841c125
Sharun 1 <sharun.1@vml.com>
2025-01-26 15:13:45 +0530

    all project done

--- app/dashboard/_components/Sidebar.js
+++ app/dashboard/_components/Sidebar.js
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { Layout, Shield } from "lucide-react";
+import { Layout,  Shield } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { Progress } from "@/components/ui/progress";
@@ -8,35 +8,52 @@ import UploadPdfDialogue from "./UploadPdfDialogue";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function Sidebar() {
-  const {user}=useUser();
-  const fileList=useQuery(api.fileStorage.GetUserFiles,{
+  const { user } = useUser();
+  const path = usePathname();
+  const GetUserInfo=useQuery(api.user.GetUserInfo,{
     userEmail:user?.primaryEmailAddress.emailAddress
   })
+  const fileList = useQuery(api.fileStorage.GetUserFiles, {
+    userEmail: user?.primaryEmailAddress.emailAddress,
+  });
+ 
   return (
     <div className="shadow-md h-screen p-7">
       <Image src={"/logo.svg"} alt="logo" width={170} height={120} />
       <div className="mt-10">
-        <UploadPdfDialogue isMaxFile={fileList?.length>=30?true:false}>
-        <Button className="w-full">+ Upload PDF</Button>
+        <UploadPdfDialogue isMaxFile={(fileList?.length >= 30 && !GetUserInfo?.upgrade) ? true : false}>
+          <Button className="w-full">+ Upload PDF</Button>
         </UploadPdfDialogue>
-        <div className="flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 rounded-lg cursor-pointer">
+        <Link href="/dashboard">
+        <div
+          className={`flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 rounded-lg cursor-pointer ${path == "/dashboard" && "bg-slate-200"} `}
+        >
           <Layout />
           <h2>Workspace</h2>
         </div>
-        <div className="flex gap-2 items-center p-3 mt-1 hover:bg-slate-100 rounded-lg cursor-pointer">
-        <Shield />
-        <h2>Upgrade</h2>
+        </Link>
+        <Link href="/dashboard/upgrade">
+        <div
+          className={`flex gap-2 items-center p-3 mt-1 hover:bg-slate-100 rounded-lg cursor-pointer ${path == "/dashboard/upgrade" && "bg-slate-200"}`}
+        >
+          <Shield />
+          <h2>Upgrade</h2>
+        </div>
+        </Link>
       </div>
-      </div>
-      <div className="absolute bottom-24 w-[80%]">
-        <Progress value={(fileList?.length/30)*100} />
-        <p className="text-sm mt-1">{fileList?.length} out of 30 Pdf Uploaded</p>
-        <p className="text-sm text-gray-400 mt-2">Upgrade to Upload more PDF </p>
-      </div>
-
-      
+     {!GetUserInfo?.upgrade&& <div className="absolute bottom-24 w-[80%]">
+        <Progress value={(fileList?.length / 30) * 100} />
+        <p className="text-sm mt-1">
+          {fileList?.length} out of 30 Pdf Uploaded
+        </p>
+        <p className="text-sm text-gray-400 mt-2">
+          Upgrade to Upload more PDF{" "}
+        </p>
+      </div>}
     </div>
   );
 }
--- convex/user.js
+++ convex/user.js
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const  createUser=mutation({
    args:{
@@ -13,12 +13,41 @@ export const  createUser=mutation({
          await ctx.db.insert('users',{
             email:args.email,
             userName:args.userName,
-            imageUrl:args.imageUrl
+            imageUrl:args.imageUrl,
+            upgrade:false
         });
         return 'Inserted New User';
     }
 
     return 'User Already Exists';
 
+   }
+})
+
+
+export const  userUpgradePlan=mutation({
+   args:{
+      userEmail:v.string()
+   },
+   handler:async(ctx,args)=>{
+       const result=await ctx.db.query('users').filter(q=>q.eq(q.field('email'),args.userEmail)).collect();
+       if(result){
+         await ctx.db.patch(result[0]._id,{upgrade:true})
+         return 'Success'
+       }
+
+       return 'Error'
+   }
+});
+
+export const GetUserInfo=query({
+   args:{
+      userEmail:v.optional(v.string()) 
+   }
+   ,
+   handler:async(ctx,args)=>{
+      if(!args?.userEmail) return;
+      const result=await ctx.db.query('users').filter(q=>q.eq(q.field('email'),args?.userEmail)).collect();
+      return result[0];
    }
 })
