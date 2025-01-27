cd7b7a4a9217ea3cd4904236d3391d53c980460a
Sharun <101327895+SHARUN266@users.noreply.github.com>
2025-01-27 18:03:28 +0530

    time added

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -7,20 +7,20 @@ import Link from 'next/link';
 import React from 'react'
 
 function Dashboard() {
-  const {user}=useUser();
-  const fileList=useQuery(api.fileStorage.GetUserFiles,{
-    userEmail:user?.primaryEmailAddress.emailAddress
+  const { user } = useUser();
+  const fileList = useQuery(api.fileStorage.GetUserFiles, {
+    userEmail: user?.primaryEmailAddress.emailAddress
   })
-  
+
   return (
     <div>
       <h2 className='font-md text-3xl'>Workspace</h2>
 
-   
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-10'>
-          {fileList?.length>0?fileList?.map((file,index)=>{
-            return(
-              <Link href={`/workspace/${file?.fileId}`} key={index}>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-10'>
+        {fileList?.length > 0 ? fileList?.map((file, index) => {
+          return (
+            <Link href={`/workspace/${file?.fileId}`} key={index}>
               <div key={index} className='flex p-5 shadow-md rounded-md flex-col items-center justify-center border cursor-pointer hover:scale-105 transition-all'>
                 <Image src={'/pdf.png'} alt='file' width={50} height={50} />
                 <h2 className='mt-3 font-medium text-md'>
@@ -29,24 +29,24 @@ function Dashboard() {
                   }
                 </h2>
                 <h2>
-  {file?._creationTime
-    ? new Date(file?._creationTime / 1000).toLocaleString()
-    : "No D&T"}
-</h2>
+                  {file?._creationTime
+                    ? new Date(file?._creationTime / 1000).toLocaleString()
+                    : "No D&T"}
+                </h2>
               </div>
-              </Link>
-            )
-          }):[1,2,3,4,5,6,7].map((item,index)=>(
-            <div key={index} className='bg-slate-200 rounded-md h-[150px]  animate-pulse'>
-            
+            </Link>
+          )
+        }) : [1, 2, 3, 4, 5, 6, 7].map((item, index) => (
+          <div key={index} className='bg-slate-200 rounded-md h-[150px]  animate-pulse'>
 
-            </div>
-          ))
-         
-        
+
+          </div>
+        ))
+
+
         }
-        </div>
-     
+      </div>
+
 
     </div>
   )
