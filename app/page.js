66aa48080665380d277c95b4dd1319b883a2ce7a
Sharun 1 <sharunattari266@gmail.com>
2025-02-22 23:59:48 +0530

    added host1

--- app/page.js
+++ app/page.js
@@ -21,11 +21,7 @@ export default function Home() {
       userName: user?.fullName,
     });
     // console.log(result);
-  };
-
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-  
+  };  
   return (
     <div>
       <section className="px-2 py-32 bg-white md:px-0">
